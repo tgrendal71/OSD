a34a655505ee20050908807b353549a7f928439f
tgrendal71 <41505917+tgrendal71@users.noreply.github.com>
2023-03-28 13:09:47 +0200

    Update start.ps1

--- cust1/start.ps1
+++ cust1/start.ps1
@@ -1,1 +1,23 @@
 Write-Host  -ForegroundColor Yellow "Starting Custom OSD ....."
+cls
+Write-Host "===================== Main Menu =======================" -ForegroundColor Yellow
+Write-Host "=================== www.evidi.com =====================" -ForegroundColor Yellow
+Write-Host "=======================================================" -ForegroundColor Yellow
+
+Write-Host "1: Windows 10 x64 (22H2) | Norsk | Enterprise "  -ForegroundColor Green
+Write-Host "2: Windows 11 x64 (22H2) | Norsk | Enterprise " -ForegroundColor Green
+Write-Host "8: Start-OSDCloud" -ForegroundColor Green
+Write-Host "9: Exit`n"-ForegroundColor Yellow
+$input = Read-Host "Please make a selection"
+
+Write-Host "Loading OSDCloud..." -BackgroundColor Gray
+Import-Module OSD -Force
+Install-Module OSD -Force
+
+switch ($input)
+{
+    '1' { Start-OSDCloud -OSLanguage da-dk -OSVersion "Windows 10" -OSBuild 22H2 -OSEdition Enterprise -ZTI } 
+    '2' { Start-OSDCloud -OSLanguage da-dk -OSVersion "Windows 11" -OSBuild 22H2 -OSEdition Enterprise -ZTI } 
+    '8' { Start-OSDCloud	} 
+    '9' { Exit		}
+}
