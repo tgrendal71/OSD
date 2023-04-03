da36e82aecb398da61de6b5a59e7d055f8e93f77
tgrendal71 <41505917+tgrendal71@users.noreply.github.com>
2023-04-03 20:00:31 +0200

    Update startup.ps1

--- bewi/startup.ps1
+++ bewi/startup.ps1
@@ -341,9 +341,6 @@ $VerbosePreference = "SilentlyContinue"
 
 [array]$MenyValg = @(
 "1: Zero-Touch Win10 22H2      | Standard BEWI image     | Svensk",
-#"2: Zero-Touch Win11 22H2      | Standard BEWI image     | Svensk",
-#"4: Zero-Touch Win10 22H2      | Standard BEWI image     | Engelsk",
-#"5: Zero-Touch Win11 22H2      | Standard BEWI image     | Engelsk",
 "8: Start OSDCloud GUI (custom)| ",
 "9: Exit"
 )
