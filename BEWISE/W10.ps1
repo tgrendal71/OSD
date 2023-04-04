4daf1b2d97fb1363f7d1b4c6728aa3bbd0503553
tgrendal71 <41505917+tgrendal71@users.noreply.github.com>
2023-04-04 21:32:43 +0200

    Create W10.ps1

---
+++ BEWISE/W10.ps1
@@ -0,0 +1,13 @@
+#================================================
+#   [PreOS] Update Module
+#================================================
+Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
+Install-Module OSD -Force
+
+Write-Host  -ForegroundColor Green "Importing OSD PowerShell Module"
+Import-Module OSD -Force
+
+if ((Get-MyComputerModel) -match 'Virtual') {
+    Write-Host  -ForegroundColor Green "Setting Display Resolution to 1600x"
+    Set-DisRes 1600
+}
