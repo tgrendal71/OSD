70c3e313c2436fc2fb0c6ed5649e5b4ff49fa7d7
tgrendal71 <41505917+tgrendal71@users.noreply.github.com>
2023-04-04 14:44:54 +0200

    Update BEWISE_DEV.ps1

--- bewi/BEWISE_DEV.ps1
+++ bewi/BEWISE_DEV.ps1
@@ -1,29 +1,25 @@
 #================================================
-#   [PreOS] Update Module
-#================================================
-Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
+Write-Host -ForegroundColor Green “Starting OSDCloud ZTI”
+
+Start-Sleep -Seconds 5
+
+#Change Display Resolution for Virtual Machine
+
+if ((Get-MyComputerModel) -match ‘Virtual’) {
+
+Write-Host -ForegroundColor Green “Setting Display Resolution to 1600x”
+
+Set-DisRes 1600
+
+}
+
+#Make sure I have the latest OSD Content
+
+Write-Host -ForegroundColor Green “Updating OSD PowerShell Module”
+
 Install-Module OSD -Force
 
-Write-Host  -ForegroundColor Green "Importing OSD PowerShell Module"
-Import-Module OSD -Force
-
-if ((Get-MyComputerModel) -match 'Virtual') {
-    Write-Host  -ForegroundColor Green "Setting Display Resolution to 1600x"
-    Set-DisRes 1600
-}
-
-#=======================================================================
-#   [OS] Params and Start-OSDCloud
-#=======================================================================
-$Params = @{
-    OSVersion = "Windows 10"
-    OSBuild = "22H2"
-    OSEdition = "Enterprise"
-    OSLanguage = "sv-se"
-    ZTI = $true
-    Firmware = $false
-}
-Start-OSDCloud @Params
+Write-Host -ForegroundColor Green “Importing OSD PowerShell Module”
 
 #================================================
 #  [PostOS] SetupComplete CMD Command Line
@@ -32,7 +28,24 @@ Write-Host -ForegroundColor Green "Create .\OSDCloud\Config\Scripts\copyapprofil
 $SetupCompleteCMD = @'
 RD C:\OSDCloud\OS /S /Q
 XCOPY '.\OSDCloud\Config\Scripts\Files\BEWI SE Standard Deployment.json'  .\OSDCloud\Config\Autopilot.json  /E /H /C /I /Y
-
-
 '@
 $SetupCompleteCMD | Out-File -FilePath '.\OSDCloud\Config\Scripts\\SetupComplete.cmd' -Encoding ascii -Force
+
+
+Import-Module OSD -Force
+
+#Start OSDCloud ZTI the RIGHT way
+
+Write-Host -ForegroundColor Green “Start OSDCloud”
+
+Start-OSDCloud -OSLanguage da-dk -OSBuild 20H2 -OSEdition Enterprise -ZTI
+
+#Restart from WinPE
+
+Write-Host -ForegroundColor Green “Restarting in 20 seconds!”
+
+Start-Sleep -Seconds 20
+
+wpeutil reboot
+
+
